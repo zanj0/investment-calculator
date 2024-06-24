b0a6b250b516293f77c33f25b2d949d4db3205a7
Agnibha Chakraborty <agnichak@amazon.com>
2024-06-24 19:12:51 +0530

    Add user input component

---
+++ src/components/UserInput.jsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+
+export default function UserInput() {
+  const [userInput, setUserInput] = useState({
+    intialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((previousUserInput) => {
+      return {
+        ...previousUserInput,
+        [inputIdentifier]: newValue,
+      };
+    });
+  }
+
+  return (
+    <section id="user-input">
+      <div className="input-group">
+        <p>
+          <label>Initial Investment</label>
+          <input
+            type="number"
+            required
+            value={userInput.intialInvestment}
+            onChange={(event) =>
+              handleChange("intialInvestment", event.target.value)
+            }
+          />
+        </p>
+        <p>
+          <label>Annual Investment</label>
+          <input
+            type="number"
+            required
+            value={userInput.annualInvestment}
+            onChange={(event) =>
+              handleChange("annualInvestment", event.target.value)
+            }
+          />
+        </p>
+      </div>
+
+      <div className="input-group">
+        <p>
+          <label>Expected Return</label>
+          <input
+            type="number"
+            required
+            value={userInput.expectedReturn}
+            onChange={(event) =>
+              handleChange("expectedReturn", event.target.value)
+            }
+          />
+        </p>
+        <p>
+          <label>Duration</label>
+          <input
+            type="number"
+            required
+            value={userInput.duration}
+            onChange={(event) => handleChange("duration", event.target.value)}
+          />
+        </p>
+      </div>
+    </section>
+  );
+}
